f27ff0b7c4d66d907eaf0e4c5c032b40cdeaa388
Eileen McNaughton <emcnaughton@wikimedia.org>
2022-01-10 14:37:43 +1300

    Formatting

--- tests/phpunit/CRM/CivitokenTest.php
+++ tests/phpunit/CRM/CivitokenTest.php
@@ -42,7 +42,7 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
    * Test token hook function works.
    */
   public function testTokenHook() {
-    $tokens = array();
+    $tokens = [];
     civitoken_civicrm_tokens($tokens);
     $this->assertTrue(!empty($tokens));
     $this->assertEquals('Address Block', $tokens['address']['address.address_block']);
@@ -56,7 +56,7 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
    */
   public function testTokenHookAlteredBySetting() {
     $tokens = [];
-    $this->callAPISuccess('Setting', 'create', array('civitoken_enabled_tokens' => array('address.address_block')));
+    $this->callAPISuccess('Setting', 'create', ['civitoken_enabled_tokens' => ['address.address_block']]);
     civitoken_civicrm_tokens($tokens);
     $this->assertEquals(['address' => ['address.address_block' => 'Address Block']], $tokens);
   }
@@ -72,7 +72,7 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
     $this->assertNotEmpty($tokens);
 
     $relationships = relationships_get_relationship_list();
-    foreach($relationships as $id => $label) {
+    foreach ($relationships as $id => $label) {
       $this->assertEquals($label . ' : Name of first contact found', $tokens['relationships']['relationships.display_name_' . $id]);
       $this->assertEquals($label . ' : First Name of first contact found', $tokens['relationships']['relationships.first_name_' . $id]);
       $this->assertEquals($label . ' : Last Name of first contact found', $tokens['relationships']['relationships.last_name_' . $id]);
@@ -83,7 +83,8 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
   }
 
   /**
-   * Test token hook function is limited if a setting is used in this case for relationship.
+   * Test token hook function is limited if a setting is used in this case for
+   * relationship.
    *
    * @throws \CiviCRM_API3_Exception
    * @throws \CRM_Core_Exception
@@ -92,12 +93,12 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
     $tokens = [];
     $tokens_to_enable = [];
     $relationships = relationships_get_relationship_list();
-    foreach($relationships as $id => $label) {
-      $tokens_to_enable[] = 'relationships.first_name_'.$id;
+    foreach ($relationships as $id => $label) {
+      $tokens_to_enable[] = 'relationships.first_name_' . $id;
     }
     $this->callAPISuccess('Setting', 'create', ['civitoken_enabled_tokens' => $tokens_to_enable]);
     civitoken_civicrm_tokens($tokens);
-    foreach($relationships as $id => $label) {
+    foreach ($relationships as $id => $label) {
       $this->assertEquals($label . ' : First Name of first contact found', $tokens['relationships']['relationships.first_name_' . $id]);
     }
   }
@@ -105,8 +106,9 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
   /**
    * Test contribution tokens for a contact with no contributions.
    *
-   * This tests that regression from https://github.com/eileenmcnaughton/nz.co.fuzion.civitoken/pull/18
-   * whereby an exception was thrown for contact without contributions stays fixed.
+   * This tests that regression from
+   * https://github.com/eileenmcnaughton/nz.co.fuzion.civitoken/pull/18 whereby
+   * an exception was thrown for contact without contributions stays fixed.
    *
    * @throws \CRM_Core_Exception
    */
@@ -120,13 +122,22 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
     $this->ids['contact'][0] = $this->individualCreate();
     $this->ids['contact'][1] = $this->individualCreate();
     $this->ids['contribution'][0] = $this->callAPISuccess('Contribution', 'create', [
-      'api.ContributionSoft.create' => ['amount' => 5, 'contact_id' => $this->ids['contact'][1], 'soft_credit_type_id' => 'in_memory_of'],
+      'api.ContributionSoft.create' => [
+        'amount' => 5,
+        'contact_id' => $this->ids['contact'][1],
+        'soft_credit_type_id' => 'in_memory_of',
+      ],
       'total_amount' => 10,
       'contact_id' => $this->ids['contact'][0],
       'financial_type_id' => 'Donation',
     ]);
     $values = [];
-    civitoken_civicrm_tokenValues($values, [$this->ids['contact'][0]], NULL, ['latestcontribs' => ['softcredit_name', 'softcredit_type']]);
+    civitoken_civicrm_tokenValues($values, [$this->ids['contact'][0]], NULL, [
+      'latestcontribs' => [
+        'softcredit_name',
+        'softcredit_type',
+      ],
+    ]);
     $this->assertEquals('In Memory of', $values[$this->ids['contact'][0]]['latestcontribs.softcredit_type']);
   }
 
@@ -137,7 +148,12 @@ class CRM_CivitokenTest extends BaseUnitTestClass implements HeadlessInterface, 
    */
   public function testAddressToken() {
     $this->ids['contact'][0] = $this->individualCreate();
-    $this->callAPISuccess('Address', 'create', ['contact_id' => $this->ids['contact'][0], 'city' => 'Baltimore', 'state_province_id' => 'Maine', 'country_id' => 'US']);
+    $this->callAPISuccess('Address', 'create', [
+      'contact_id' => $this->ids['contact'][0],
+      'city' => 'Baltimore',
+      'state_province_id' => 'Maine',
+      'country_id' => 'US',
+    ]);
     $tokens = ['address.address_block'];
     $values = $this->processTokens($tokens);
     $this->assertEquals([
@@ -152,13 +168,15 @@ UNITED STATES<br />
 ',
     ], $values[$this->ids['contact'][0]]
     );
-    $this->callAPISuccess('Setting', 'create', ['mailing_format' => '{contact.addressee}
+    $this->callAPISuccess('Setting', 'create', [
+      'mailing_format' => '{contact.addressee}
 {contact.street_address}
 {contact.supplemental_address_1}
 {contact.city}
 {contact.state_province_name}
 {contact.postal_code}
-{contact.country}']);
+{contact.country}',
+    ]);
     $values = $this->processTokens($tokens);
     $this->assertEquals('bob
 Baltimore
@@ -175,7 +193,10 @@ UNITED STATES
    * @throws \CRM_Core_Exception
    */
   protected function individualCreate(): int {
-    return (int) $this->callAPISuccess('Contact', 'create', ['contact_type' => 'Individual', 'first_name' => 'bob'])['id'];
+    return (int) $this->callAPISuccess('Contact', 'create', [
+      'contact_type' => 'Individual',
+      'first_name' => 'bob',
+    ])['id'];
   }
 
   /**
