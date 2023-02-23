72d38b4ea423a99a1ebe8a62324f34b7b2a0ce04
Eileen McNaughton <emcnaughton@wikimedia.org>
2023-02-23 17:05:39 +1300

    Ciix upgrade

--- civitoken.php
+++ civitoken.php
@@ -30,59 +30,12 @@ function civitoken_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function civitoken_civicrm_postInstall() {
-  _civitoken_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function civitoken_civicrm_uninstall() {
-  _civitoken_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_enable
  */
 function civitoken_civicrm_enable() {
   _civitoken_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function civitoken_civicrm_disable() {
-  _civitoken_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function civitoken_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _civitoken_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function civitoken_civicrm_entityTypes(&$entityTypes) {
-  _civitoken_civix_civicrm_entityTypes($entityTypes);
 }
 
 // --- Functions below this ship commented out. Uncomment as required. ---
