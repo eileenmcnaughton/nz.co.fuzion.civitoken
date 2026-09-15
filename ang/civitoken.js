60582251645bf29f276572e017c1205e4ffd9af9
colemanw <coleman@civicrm.org>
2026-09-15 15:52:57 -0400

    Remove unused lo-dash (CRM._)
    Lo-dash is deprecated and will someday be removed from CiviCRM-core.
    This improves forward-compatibility by dropping unused references to it.

--- ang/civitoken.js
+++ ang/civitoken.js
@@ -1,6 +1,6 @@
-(function(angular, $, _) {
+(function(angular, $) {
   // Declare a list of dependencies.
   angular.module('civitoken', [
     'crmUi', 'crmUtil', 'ngRoute'
   ]);
-})(angular, CRM.$, CRM._);
+})(angular, CRM.$);
